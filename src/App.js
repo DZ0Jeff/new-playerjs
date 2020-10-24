2dab4a8dd2f6ff084270c8c36bbaa672248678e9
Jeferson <mx,jeferson.12@gmail.com>
2020-10-24 01:01:35 -0300

    refactor player

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 
 // Css
@@ -9,16 +9,34 @@ import './globals.css';
 import { Container, Row } from 'react-bootstrap'
 
 function App() {
-  const music = "music/opeth.mp3"
+  let [currentMusic, setCurrentMusic] = useState(0) 
+
+  let playlist = [
+    { name: 'opeth', src: "music/opeth.mp3" },
+    { name: 'Motorhead - Ace of spades', src: "music/Ace of Spades.mp3" },
+    { name: 'Black sabbath - paranoid', src: "music/paranoid.mp3" }
+  ]
+
+  function handleClickNext(){
+    return currentMusic >= playlist.length - 1 ? setCurrentMusic(currentMusic = 0) : setCurrentMusic(currentMusic + 1)
+  }
+
+  function handleClickPrev(){
+    return currentMusic === 0 ? setCurrentMusic(currentMusic + playlist.length - 1) : setCurrentMusic(currentMusic - 1)
+  }
   
   return (
     <div className="main-site">
       <Container>
         <Row>
           <div className="col-lg-12 mx-auto">
+            <h1 className="text-center">{playlist[currentMusic].name}</h1>
             <AudioPlayer
+              autoPlayAfterSrcChange={false}
               showSkipControls={true}
-              src={music}
+              src={playlist[currentMusic].src}
+              onClickNext={handleClickNext}
+              onClickPrevious={handleClickPrev}
             />
           </div>
         </Row>
